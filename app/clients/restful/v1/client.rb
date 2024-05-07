749b904ed0d940c2f08239cbed48baade21bfc5f
p3rc1us <kervymaniego@gmail.com>
2024-05-07 17:24:09 +0800

    added specific_object method

--- app/clients/restful/v1/client.rb
+++ app/clients/restful/v1/client.rb
@@ -9,19 +9,32 @@ class Restful::V1::Client
     )
   end
 
+  def specific_object(id)
+    request(
+      method: :get,
+      endpoint: "objects/#{id}"
+    )
+  end
 
   private
 
   def request(method:, endpoint:, headers: {}, body: {})
-    response = connection.public_send(method, "#{endpoint}") do |request|
-      request.headers = headers if headers.present?
-      request.body = body.to_json if body.present?
-    end
-
-    if response.success?
-      JSON.parse(response.body).map { |obj| obj.with_indifferent_access }
-    end
+  response = connection.public_send(method, "#{endpoint}") do |request|
+     request.headers = headers if headers.present?
+     request.body = body.to_json if body.present?
   end
+
+  if response.success?
+     begin
+       JSON.parse(response.body).with_indifferent_access
+     rescue JSON::ParserError => e
+       Rails.logger.error "Failed to parse JSON: #{e.message}"
+       nil
+     end
+  else
+     nil
+  end
+ end
 
   def connection
     @connection ||= Faraday.new(url: BASE_URL)
