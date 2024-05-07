4f5a962c69ae4a3ff93b9384b7a0a79b337b0842
p3rc1us <kervymaniego@gmail.com>
2024-05-07 17:24:21 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
+
+  get 'pages/index'
+
+  get 'pages/select', to: 'pages#select', as: 'select'
+
+  get 'pages/show', to: 'pages#show'
 
   resources :objects
 end
