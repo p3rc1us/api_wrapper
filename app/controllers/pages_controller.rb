cedb4c81839889143b203c57565070e4f5c86b41
p3rc1us <kervymaniego@gmail.com>
2024-05-07 17:24:32 +0800

    modified controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
     @objects = client.objects
   end
 
-  def show
-  end
+  def select
+    client = Restful::V1::Client.new
+    @object_id = params[:object_id]
+    @object = client.specific_object(@object_id)
+ end
+
 end
